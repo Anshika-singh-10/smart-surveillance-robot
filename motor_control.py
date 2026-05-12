c5b9eb46a1efcf57fdfad7e56e68af8570e187ee
Anshika Singh <th.anshika1006@gmail.com>
2026-05-12 21:19:30 +0530

    Create motor_control.py

---
+++ motor_control.py
@@ -0,0 +1,16 @@
+# motor_control.py
+
+def move_forward():
+    print("Robot Moving Forward")
+
+def move_backward():
+    print("Robot Moving Backward")
+
+def turn_left():
+    print("Robot Turning Left")
+
+def turn_right():
+    print("Robot Turning Right")
+
+def stop_robot():
+    print("Robot Stopped")
